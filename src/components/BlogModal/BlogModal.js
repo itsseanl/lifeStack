0d51dcaacd791062b9554e549275057daa370c14
Sean Lyons <seanlyons@Seans-MacBook-Pro.local>
2019-02-14 12:41:11 -0500

    added recommendation ads link

--- src/components/BlogModal/BlogModal.js
+++ src/components/BlogModal/BlogModal.js
@@ -16,7 +16,9 @@ const BlogModal = ({ info, handleClose, showModal }) => {
         <div className="blogModalText">
           <h1 className="item">{info[0].title}</h1>
           <h3 className="price">{info[0].date}</h3>
-          <div dangerouslySetInnerHTML={{__html: info[0].text }}></div>
+          <div dangerouslySetInnerHTML={{__html: info[0].text }}>
+          </div>
+<div id="amzn-assoc-ad-b24487a7-11a0-4c3d-91a1-5edca93e6e67"></div><script async src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=b24487a7-11a0-4c3d-91a1-5edca93e6e67"></script>
         </div>
         <button  className="btnClose" onClick={handleClose}><i class="fas fa-times"></i></button>
         </article>
